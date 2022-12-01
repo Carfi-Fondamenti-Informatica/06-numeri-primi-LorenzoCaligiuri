8b4f7f4501214c55f8bec0f64dbe4ee7e5a935c8
LorenzoCaligiuri <115158644+LorenzoCaligiuri@users.noreply.github.com>
2022-12-01 11:45:48 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,13 +1,12 @@
 #include "lib.h"
 
-bool Nprimi(int n, int div){
-    for(int i=n-1; i>=1; i--){
-        if((n%div==0) and (div!=n)){
-            return false;
-        }else if((div=1) and (n%div==0)){
-            return true;
-        }else{
-            Nprimi(n,--div);
-        }
+bool Nprimi(int div, int n) {
+    if(div==1){
+        return true;
+    }else if(n%div==0){
+        return false;
+    }else{
+        Nprimi(--div, n);
     }
+
 }
